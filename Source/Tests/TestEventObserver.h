4edd57f0001e83eedbf3d4fa0ae1914327c390f0
Halcom1979 <halcom@gmx.de>
2017-11-23 15:13:41 +0100

    new: Added TestObserver to check events in tests

--- Source/Tests/TestEventObserver.h
+++ Source/Tests/TestEventObserver.h
@@ -1,4 +1,40 @@
 #ifndef TESTEVENTOBSERVER_H
 #define TESTEVENTOBSERVER_H
 
+#include "Macros.h"
+#include "EventProvider.h"
+
+class TestEventObserver: public EventObserver
+{
+  public:
+    TestEventObserver(EventProvider * prov, EventType type, EntityId id)
+    : mEventProvider(prov)
+    , mEventType(type)
+    , mId(id)
+    , mReceived(false)
+    {
+      dbg_assert(prov != nullptr);
+      mEventProvider->addObserver(this);
+    }
+
+    ~TestEventObserver() { mEventProvider->removeObserver(this); }
+
+    void handleEvent(EventType t, EntityId id) {
+      if(mEventType == t && mId == id) {
+        mReceived = true;
+      }
+    }
+
+    bool received() { return mReceived; }
+    void clear() { mReceived = false; }
+
+  private:
+    DISABLE_DFLT_CTOR(TestEventObserver)
+
+    EventProvider * mEventProvider;
+    EventType       mEventType;
+    EntityId        mId;
+    bool            mReceived;
+};
+
 #endif // TESTEVENTOBSERVER_H
